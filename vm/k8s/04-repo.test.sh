49ffcccb10059f3ea4b5a9d27e470bc8aea0d8f3
repocurso <repocurso@curso.es>
2024-07-12 15:06:17 +0200

    Creación Repositorio - entornos

--- vm/k8s/04-repo.test.sh
+++ vm/k8s/04-repo.test.sh
@@ -3,6 +3,14 @@
 export KUBECONFIG=kubeconfig/config
 
 kubectl create deployment deploy1 --image=repocurso:9091/nginx --replicas 3
-kubectl get pods -l app=deploy1 --watch
+kubectl get pods -owide -l app=deploy1 --watch
 kubectl delete deployment deploy1
+
+for node in master node1 node2; do
+ssh vagrant@$node sh -c 'cat << EOT | sudo sh -
+sudo crictl -r unix:///var/run/crio/crio.sock  images
+sudo crictl -r unix:///var/run/crio/crio.sock  rmi repocurso:9091/nginx
+sudo crictl -r unix:///var/run/crio/crio.sock  images
+EOT';
+done
 
