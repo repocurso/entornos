2417fbc081b758130fef9a1424416f18c0fa81da
repocurso <repocurso@curso.es>
2024-07-13 02:29:43 +0200

    Creación Repositorio - entornos

--- vm/swarm/04-repo-config.sh
+++ vm/swarm/04-repo-config.sh
@@ -3,10 +3,10 @@
 for node in manager worker01 worker02; do
 ssh vagrant@$node sh -c 'cat << EOT | sudo sh - 
 sed -i "/repocurso/d" /etc/hosts
-echo "\$a 192.168.100.200\trepocurso.local\trepocurso" /etc/hosts
-#cat > /etc/docker/daemon.json << EOF
-#{ "insecure-registries": ["192.168.100.200:9091","repocurso:9091"] }
-#EOF
-#systemctl restart docker
+echo "192.168.100.200\trepocurso.local\trepocurso" >> /etc/hosts
+cat > /etc/docker/daemon.json << EOF
+{ "insecure-registries": ["192.168.100.200:9091","repocurso:9091"] }
+EOF
+systemctl restart docker
 EOT';
 done
