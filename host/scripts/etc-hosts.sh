4c67813a9df934f16710f6d46bc0ee77dc8722cc
repocurso <repocurso@curso.es>
2024-07-14 10:35:28 +0200

    Creación Repositorio - entornos

--- host/scripts/etc-hosts.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-echo "192.168.100.200 repocurso" >> /etc/hosts
-sed -i -e '$a10.100.199.200  manager' -e '$a10.100.199.201  worker01' -e '$a10.100.199.202  worker02' /etc/hosts
-sed -i -e '$a10.0.0.10       master'  -e '$a10.0.0.11       node1'    -e '$a10.0.0.12       node2'    /etc/hosts
-
