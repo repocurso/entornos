07a2be14b8510630081ec20d868b762e293c835e
repocurso <repocurso@curso.es>
2024-07-14 18:32:34 +0200

    Creación Repositorio - entornos

--- host/02-docker-install.sh
+++ host/02-docker-install.sh
@@ -1,20 +1,19 @@
 #!/bin/bash
 
 # ----- Desinstalacion de docker
+
 sudo yum remove -y docker docker-client docker-client-latest docker-common docker-latest docker-latest-logrotate docker-logrotate docker-engine
 sudo yum remove -y docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin docker-ce-rootless-extras
 sudo rm -rf /var/lib/docker 
 sudo rm -rf /var/lib/containerd
 
 # ----- Instalacion de docker
+
 sudo yum install -y yum-utils
-
 sudo yum-config-manager --add-repo https://download.docker.com/linux/centos/docker-ce.repo
-
 sudo yum makecache -y fast
 
 sudo yum install -y docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin docker-ce-rootless-extras
-
 sudo systemctl start docker
 sudo systemctl enable docker
 
