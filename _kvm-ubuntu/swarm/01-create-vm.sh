630d7a58254a7cc0260348f7f0e12fe2c4b87892
Ubuntu <pue@labAJQTMA.neepgcdmcile1j3r1e23zcwdke.ax.internal.cloudapp.net>
2024-07-19 11:59:43 +0200

    Creación Repositorio - entornos

--- _kvm-ubuntu/swarm/01-create-vm.sh
+++ _kvm-ubuntu/swarm/01-create-vm.sh
@@ -2,3 +2,11 @@
 
 vagrant up --provider=libvirt --no-parallel
 
+# --- Renombrar VM (kvm/libvirt)
+#virsh shutdown swarm_manager
+#virsh shutdown swarm_worker01
+#virsh shutdown swarm_worker02
+#sleep 30
+#virsh domrename swartu_ubuntu docker-ubuntu
+#virsh start docker-ubuntu
+
