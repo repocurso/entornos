f803c4cb8bd23603121fd3981fac0b352c6d51db
Ubuntu <pue@labAJQTMA.neepgcdmcile1j3r1e23zcwdke.ax.internal.cloudapp.net>
2024-07-19 21:51:05 +0200

    Creación Repositorio - entornos

--- _kvm-ubuntu/start-env/stopvm-swarm.sh
+++ _kvm-ubuntu/start-env/stopvm-swarm.sh
@@ -4,8 +4,4 @@
 virsh shutdown swarm_worker02 --mode acpi 
 virsh shutdown swarm_worker01 --mode acpi 
 virsh shutdown swarm_manager --mode acpi 
-sleep 10
-virsh start swarm_manager
-virsh start swarm_worker01
-virsh start swarm_worker02
 
